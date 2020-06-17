757db12fd40465e3c28f0cba91027a0cf7db353e
sakthi <sakthivelit059@gmail.com>
2020-06-17 08:14:43 +0530

    Updates

---
+++ precache-manifest.14020c2a017f382fda856eef1c61e774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f0e80ce2b0af89b7da776a9fe36bdf",
+    "url": "/sakthivelgovindan/index.html"
+  },
+  {
+    "revision": "10d2472a4097251c6902",
+    "url": "/sakthivelgovindan/static/css/main.7985e30c.chunk.css"
+  },
+  {
+    "revision": "3502ed5fcbc42045f08f",
+    "url": "/sakthivelgovindan/static/js/2.c206fde0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sakthivelgovindan/static/js/2.c206fde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d2472a4097251c6902",
+    "url": "/sakthivelgovindan/static/js/main.5987f98c.chunk.js"
+  },
+  {
+    "revision": "7cfce5b6845c8245b8e1",
+    "url": "/sakthivelgovindan/static/js/runtime-main.154edfeb.js"
+  }
+]);
